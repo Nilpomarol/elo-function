57e1524b193fa38c095e9eb59a307e06965c8c36
Nilpomarol <nilpomaroltur@gmail.com>
2024-05-25 09:38:10 +0200

    millores codi i interficie i proves dels partits normal i partits americanes

--- elo.py
+++ elo.py
@@ -4,70 +4,50 @@ import streamlit as st
 import pandas as pd
 
 
-def update_elo_2v2(team1_ratings, team2_ratings, result, K=32, ratio = 400, base = 50):
+def update_elo_2v2(team1_ratings, team2_ratings, result, K=32, ratio=400, base=50):
     """
-    Calcula los nuevos puntajes Elo de los jugadores después de un partido 2 vs 2.
+    Calculate new Elo ratings for two teams based on the outcome of a match.
 
     Args:
-        team1_ratings (list): Lista de puntajes Elo de los jugadores en el equipo 1.
-        team2_ratings (list): Lista de puntajes Elo de los jugadores en el equipo 2.
-        result (list): Resultado del partido [puntos anotados por equipo 1, puntos anotados por equipo 2].
-        K (int): Factor de ajuste Elo (por defecto, K = -32 para aumentar la sensibilidad).
+        team1_ratings (list): List of integers representing the Elo ratings of the first team's players.
+        team2_ratings (list): List of integers representing the Elo ratings of the second team's players.
+        result (list): List of two integers representing the scores of team 1 and team 2, respectively.
+        K (int, optional): The maximum possible adjustment per player per game. Defaults to 32.
+        ratio (int, optional): Used to scale the difference in ratings. Defaults to 400.
+        base (int, optional): A base adjustment factor that can be modified based on the rating difference. Defaults to 50.
 
     Returns:
-        tuple: Tupla con los nuevos puntajes Elo de los jugadores en cada equipo.
+        tuple: Two lists containing the new Elo ratings for the players of team 1 and team 2 respectively.
     """
-    # Calcula la probabilidad de que el equipo 1 gane
     team1_avg = sum(team1_ratings) / len(team1_ratings)
     team2_avg = sum(team2_ratings) / len(team2_ratings)
     
-    # Calcula los nuevos puntajes Elo de los jugadores en cada equipo
-    team1_new_ratings = []
-    team2_new_ratings = []
-    game_result = result[0] / (result[0] + result[1])
-    game_result = (game_result*0.8)+0.1
-
+    game_result = result[0] / sum(result)
+    
+    rating_diff = abs(team1_avg - team2_avg)
+    
     if result[0] > result[1]:
-        if (team1_avg - team2_avg) > 1000:
-            base = base - 15
-        elif (team1_avg - team2_avg) > 700:
-            base = base - 10
-        elif (team1_avg - team2_avg) > 400:
-            base = base - 5
-        for rating in team1_ratings:
-            team1_P = 1/ (1 + 1 * math.pow(10, 1 * (team2_avg - rating) / ratio))
-            team1_new_ratings.append(int(round(base+K * (game_result - team1_P))))  # Equipo 1
-        for rating in team2_ratings:
-            team2_P = 1/ (1 + 1 * math.pow(10, 1 * (team1_avg - rating) / ratio))
-            team2_new_ratings.append(int(round(-base + K * (1-game_result - team2_P))))
+        team1_factor, team2_factor = 1, -1
     else:
-        if (team2_avg - team1_avg) > 1000:
-            base = base - 15
-        elif (team2_avg - team1_avg) > 700:
-            base = base - 10
-        elif (team2_avg - team1_avg) > 400:
-            base = base - 5
-        for rating in team1_ratings:
-            team1_P = 1/ (1 + 1 * math.pow(10, 1 * (team2_avg - rating) / ratio))
-            team1_new_ratings.append(int(round(-base+K * (game_result - team1_P))))  # Equipo 1
-        for rating in team2_ratings:
-            team2_P = 1/ (1 + 1 * math.pow(10, 1 * (team1_avg - rating) / ratio))
-            team2_new_ratings.append(int(round(base+K * (1-game_result - team2_P))))
+        team1_factor, team2_factor = -1, 1
+    
+    if rating_diff > 1000:
+        base -= 15
+    elif rating_diff > 700:
+        base -= 10
+    elif rating_diff > 400:
+        base -= 5
+    
+    def calculate_new_rating(rating, opponent_avg, game_result, factor):
+        P = 1 / (1 + math.pow(10, (opponent_avg - rating) / ratio))
+        return int(round(factor * base + K * (game_result - P)))
+    
+    team1_new_ratings = [calculate_new_rating(rating, team2_avg, game_result, team1_factor) for rating in team1_ratings]
+    team2_new_ratings = [calculate_new_rating(rating, team1_avg, 1 - game_result, team2_factor) for rating in team2_ratings]
     
     return team1_new_ratings, team2_new_ratings
    
 def update_elo_american(ratings, results, K=100, ratio = 800,numpistas = 6, base = 10):
-    """
-    Calcula los nuevos puntajes Elo de los jugadores después de un torneo de sistema americano.
-
-    Args:
-        results (list): Lista de resultados de los partidos en el torneo.
-        ratings (list): Lista de puntajes Elo de los jugadores.
-        K (int): Factor de ajuste Elo (por defecto, K = -32 para aumentar la sensibilidad).
-
-    Returns:
-        list: Lista con los nuevos puntajes Elo de los jugadores.
-    """
     # Calcula los nuevos puntajes Elo de los jugadores
     new_ratings = []
     avg = 0
@@ -88,9 +68,18 @@ def update_elo_american(ratings, results, K=100, ratio = 800,numpistas = 6, base
         
         for jugador in ratings[i]:
             P = 1/ (1 + 1 * math.pow(10, 1 * (avg - jugador) / ratio))
-            if first== True:
-                print(int(round(K * (americana_result - P))))
             new_elo = int(round(K * (americana_result - P)))
+            
+            if americana_result > P:
+                elodiff = jugador - avg
+                if elodiff < -200:
+                    new_elo += base + 15
+                elif elodiff < 0:
+                    new_elo += base + 10
+                elif elodiff < 150:
+                    new_elo += base + 5
+                else:
+                    new_elo += base 
             if americana_result >= 0.5:
                 elodiff = jugador - avg
                 if elodiff < -200:
@@ -109,8 +98,8 @@ def update_elo_american(ratings, results, K=100, ratio = 800,numpistas = 6, base
                     new_elo -= base + 10
                 else:
                     new_elo -= base + 15
+                    
             if first == True:
-                print (elodiff)
                 print(new_elo)
                 print(americana_result)
                 first = False
@@ -122,63 +111,134 @@ def update_elo_american(ratings, results, K=100, ratio = 800,numpistas = 6, base
 
     return new_ratings
 
-    
+
+
+####################################################################INTERFICIE STREAMLIT####################################################################
+
+
 def page1():
-    # Ejemplo de uso
-    col1, col2 = st.columns(2)
-
-    with col1:
-        ratio = int(st.text_input("Importancia diferencia de ratings: ", "400"))  # Ratio de diferencia de puntajes Elo
-        st.write("Equipo 1:")
-        team1_elo = [int(st.text_input("jugador 1: ", "1349")), int(st.text_input("jugador 2: ", "1270"))]  # Puntajes Elo de los jugadores en el equipo 1
+    st.header("Calculadora de Puntajes Elo Partidos Normales")
+    
+    col1, col2= st.columns(2)
     with col2:
-        k = int(st.text_input("Factor de ajuste Elo: ", "20"))  # Factor de ajuste Elo
-        st.write("Equipo 2:")
-        team2_elo = [int(st.text_input("jugador 3: ", "1406")), int(st.text_input("jugador 4: ", "1402"))]  # Puntajes Elo de los jugadores en el equipo 2
-    result = st.text_input("Result: ", "3-7").split("-")
-    result = [int(result[0]), int(result[1])]
+        st.subheader("Parámetros de Elo")
+        with st.expander("Parámetros de Elo", expanded=True,):
+            ratio = st.slider("Importancia de Diferencia de Ratings (Ratio)", min_value=100, max_value=2000, value=400, step=50)
+            k = st.slider("Factor de Ajuste Elo (K)", min_value=0, max_value=100, value=20, step=2)
+            base = st.slider("Valor Base (Base)", min_value=0, max_value=100, value=50, step=1)
 
-
-    new_elo_team1, new_elo_team2 = update_elo_2v2(team1_elo, team2_elo, result, k, ratio)
-    st.write("Nuevos puntajes Elo:")
-    col1, col2 = st.columns(2)
-    with col1:
-        st.write("Equipo 1:")
-        for i, elo in enumerate(new_elo_team1):
-            st.write(f"Jugador {i+1}: {team1_elo[i]} -> {elo}")
+    if "ratings" not in st.session_state:
+        st.session_state["ratings"] = [1200, 1210, 1220, 1230]
+        st.session_state["result"] = [7, 5]
+    
     with col2:
-        st.write("Equipo 2:")
-        for i, elo in enumerate(new_elo_team2):
-            st.write(f"Jugador {i+3}: {team2_elo[i]} -> {elo}")
+        with st.expander("Parámetros de Aleatoriedad", expanded=True):
+            min_rating = st.number_input("Rating Mínimo", value=1000)
+            max_rating = st.number_input("Rating Máximo", value=1500)
+        
+        generate_button = st.button("Generar Ratings Aleatorios")   
+        if generate_button:
+            st.session_state["ratings"] = [random.randint(min_rating, max_rating) for _ in range(4)]
+            result1 = random.randint(0, 7)
+            result2 = 7 if result1 != 7 else random.randint(0, 6)
+            st.session_state["result"] = [result1, result2]
+    
+    with col1:
+        st.subheader("Entrada de Ratings y Resultados")
+        with st.expander("Explicación de Parámetros:"):
+            st.write("""
+            - **Ratio**: Determina la importancia de la diferencia de ratings entre los equipos. Un valor más alto reduce la influencia de la diferencia de ratings.
             
+            - **K**: Controla la velocidad de ajuste del rating Elo. Un valor más alto significa ajustes más rápidos.
+        
+            - **Base**: Ajuste adicional que puede influir en el cambio de rating, especialmente cuando hay grandes diferencias de rating entre equipos.
+            """)
+
+        col3, col4 = st.columns(2)
+        with col3:
+            st.write("Equipo 1:")
+            team1_elo = [int(st.text_input(f"Jugador {i+1}: ", st.session_state["ratings"][i])) for i in range(2)]
+        with col4:
+            st.write("Equipo 2:")
+            team2_elo = [int(st.text_input(f"Jugador {i+3}: ", st.session_state["ratings"][i+2])) for i in range(2)]
+            
+        result_input = st.text_input("Resultado: ", f"{st.session_state['result'][0]}-{st.session_state['result'][1]}")
+        st.session_state["result"] = list(map(int, result_input.split("-")))
+
+        new_elo_team1, new_elo_team2 = update_elo_2v2(team1_elo, team2_elo, st.session_state["result"], k, ratio, base)
+
+        data = {
+            "Jugador": ["Jugador 1", "Jugador 2", "Jugador 3", "Jugador 4"],
+            "Equipo": ["Equipo 1", "Equipo 1", "Equipo 2", "Equipo 2"],
+            "Rating Anterior": team1_elo + team2_elo,
+            "Nuevo Rating": new_elo_team1 + new_elo_team2
+        }
+
+        df = pd.DataFrame(data)
+        st.table(df)
+
 def page2():
-    # Ejemplo de uso
-    col1, col2 = st.columns(2)
-
-    with col1:
-        ratio = int(st.text_input("Importancia diferencia de ratings: ", "400"))  # Ratio de diferencia de puntajes Elo
-        st.write("Equipo 1:")
-        team1_elo = [int(st.text_input("jugador 1: ", "1349")), int(st.text_input("jugador 2: ", "1270"))]  # Puntajes Elo de los jugadores en el equipo 1
+    st.header("Calculadora de Puntajes Elo Partidos Americana")
+    
+    col1, col2= st.columns(2)
     with col2:
-        k = int(st.text_input("Factor de ajuste Elo: ", "20"))  # Factor de ajuste Elo
-        st.write("Equipo 2:")
-        team2_elo = [int(st.text_input("jugador 3: ", "1406")), int(st.text_input("jugador 4: ", "1402"))]  # Puntajes Elo de los jugadores en el equipo 2
-    result = st.text_input("Result: ", "3-7").split("-")
-    result = [int(result[0]), int(result[1])]
+        st.subheader("Parámetros de Elo")
+        with st.expander("Parámetros de Elo", expanded=True,):
+            ratio = st.slider("Importancia de Diferencia de Ratings (Ratio)", min_value=100, max_value=2000, value=400, step=50)
+            k = st.slider("Factor de Ajuste Elo (K)", min_value=0, max_value=100, value=20, step=2)
+            base = st.slider("Valor Base (Base)", min_value=0, max_value=100, value=20, step=1)
 
+        
 
-    new_elo_team1, new_elo_team2 = update_elo_2v2(team1_elo, team2_elo, result, k, ratio, base = 25)
-    st.write("Nuevos puntajes Elo:")
-    col1, col2 = st.columns(2)
-    with col1:
-        st.write("Equipo 1:")
-        for i, elo in enumerate(new_elo_team1):
-            st.write(f"Jugador {i+1}: {team1_elo[i]} -> {elo}")
+    if "ratings" not in st.session_state:
+        st.session_state["ratings"] = [1200, 1210, 1220, 1230]
+        st.session_state["result"] = [7, 5]
+    
     with col2:
-        st.write("Equipo 2:")
-        for i, elo in enumerate(new_elo_team2):
-            st.write(f"Jugador {i+3}: {team2_elo[i]} -> {elo}")
+        with st.expander("Parámetros de Aleatoriedad", expanded=True):
+            min_rating = st.number_input("Rating Mínimo", value=1000)
+            max_rating = st.number_input("Rating Máximo", value=1500)
+        
+        generate_button = st.button("Generar Ratings Aleatorios")   
+        if generate_button:
+            st.session_state["ratings"] = [random.randint(min_rating, max_rating) for _ in range(4)]
+            result1 = random.randint(0, 7)
+            result2 = 7 if result1 != 7 else random.randint(0, 6)
+            st.session_state["result"] = [result1, result2]
+    
+    with col1:
+        st.subheader("Entrada de Ratings y Resultados")
+        with st.expander("Explicación de Parámetros:"):
+            st.write("""
+            - **Ratio**: Determina la importancia de la diferencia de ratings entre los equipos. Un valor más alto reduce la influencia de la diferencia de ratings.
+            
+            - **K**: Controla la velocidad de ajuste del rating Elo. Un valor más alto significa ajustes más rápidos.
+        
+            - **Base**: Ajuste adicional que puede influir en el cambio de rating, especialmente cuando hay grandes diferencias de rating entre equipos.
+            """)
+        col3, col4 = st.columns(2)
+        with col3:
+            st.write("Equipo 1:")
+            team1_elo = [int(st.text_input(f"Jugador {i+1}: ", st.session_state["ratings"][i])) for i in range(2)]
+        with col4:
+            st.write("Equipo 2:")
+            team2_elo = [int(st.text_input(f"Jugador {i+3}: ", st.session_state["ratings"][i+2])) for i in range(2)]
+            
+        result_input = st.text_input("Resultado: ", f"{st.session_state['result'][0]}-{st.session_state['result'][1]}")
+        st.session_state["result"] = list(map(int, result_input.split("-")))
 
+        new_elo_team1, new_elo_team2 = update_elo_2v2(team1_elo, team2_elo, st.session_state["result"], k, ratio, base)
+
+        data = {
+            "Jugador": ["Jugador 1", "Jugador 2", "Jugador 3", "Jugador 4"],
+            "Equipo": ["Equipo 1", "Equipo 1", "Equipo 2", "Equipo 2"],
+            "Rating Anterior": team1_elo + team2_elo,
+            "Nuevo Rating": new_elo_team1 + new_elo_team2
+        }
+
+        df = pd.DataFrame(data)
+        st.table(df)
+       
 def page3():
     st.title("Configuración de Parámetros")
 
@@ -232,6 +292,10 @@ def page3():
 
 def page4():
 
+    if "ratings" not in st.session_state:
+        st.session_state.ratings = (1500, 1500)
+        st.session_state.results = (1, 1)
+        
     col1, col2 = st.columns([2,1])
     with col2:
         st.subheader("Configuración Parámetros")
@@ -249,15 +313,17 @@ def page4():
         generate_ratings = st.button("Ratings Aleatorios")
 
 
-        ratings = [(1500, 1500), (average, average),(average, average),(average, average),(average, average),(average, average),(average, average),(average, average),(average, average),(average, average),(average, average),(average, average),]
-        results = [(1, 1), (1,1), (2,2), (2,2), (3,3), (3,3), (4,4), (4,4), (5,5), (5,5), (6,6), (6,6)]
+        ratings = [st.session_state.ratings, (average, average),(average, average),(average, average),(average, average),(average, average),(average, average),(average, average),(average, average),(average, average),(average, average),(average, average),]
+        results = [st.session_state.results, (1,1), (2,2), (2,2), (3,3), (3,3), (4,4), (4,4), (5,5), (5,5), (6,6), (6,6)]
 
         if generate_ratings == True:
             randratings = (random.randint(average-variability, average+variability), random.randint(average-variability, average+variability))
             ratings[0] = randratings
+            st.session_state.ratings = randratings
             result = (random.randint(1,6), random.randint(1,6))
             results[0] = result
-    
+            st.session_state.results = result
+                
         with st.expander("Pareja 0: ", expanded = True):
             col3, col4 = st.columns(2)
             with col3:
